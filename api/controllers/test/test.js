f6882e8387f12c1088625ec9ecd79e10904d4b4f
Jorge Palacios <reefaktor@gmail.com>
2021-02-26 01:18:50 -0300

    Implemented endpoint /test
    * Implemented endpoint /test
    * Routed endpoint /test to root as well

---
+++ api/controllers/test/test.js
@@ -0,0 +1,14 @@
+module.exports = {
+  friendlyName: 'Test',
+
+  description: 'Test test.',
+
+  inputs: {},
+
+  exits: {},
+
+  fn: async function () {
+    // All done.
+    return { message: 'It works!' };
+  },
+};
